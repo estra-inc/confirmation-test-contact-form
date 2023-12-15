9e175f528984755c8bfc87b903ce9a31d2c74198
ychihiro <y.chihiro0510@gmail.com>
2023-12-15 11:15:24 +0900

    feat: フォームリクエスト作成

---
+++ src/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'require',
+            'email' => 'require | email',
+            'password' => 'require'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
+}
--- src/resources/views/Auth/register.blade.php
+++ src/resources/views/Auth/register.blade.php
@@ -8,14 +8,29 @@
     <label for="name">お名前</label>
       <input type="text" name="name" id="name">
   </div>
+  <p>
+    @error('name')
+    {{ $message }}
+    @enderror
+  </p>
   <div>
     <label for="email">メールアドレス</label>
       <input type="mail" name="email" id="email">
   </div>
+  <p>
+    @error('email')
+    {{ $message }}
+    @enderror
+  </p>
   <div>
     <label for="password">パスワード</label>
       <input type="password" name="password" id="password">
   </div>
+  <p>
+    @error('password')
+    {{ $message }}
+    @enderror
+  </p>
   <input type="submit" value="登録">
 </form>
 @endsection('content')
