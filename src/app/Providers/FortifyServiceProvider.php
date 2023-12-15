05e7a3d7a16f2464dac6126dd034843c3752ee9a
ychihiro <y.chihiro0510@gmail.com>
2023-12-15 14:52:36 +0900

    feat: サービスプロバイダにログイン機能の設定を追加

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -33,5 +33,15 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+        $email = (string) $request->email;
+
+        return Limit::perMinute(10)->by($email . $request->ip());
+        });
     }
 }
