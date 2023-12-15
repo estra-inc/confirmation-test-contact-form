9a080043b6718c6fabd06040c03fa6909259dd94
ychihiro <y.chihiro0510@gmail.com>
2023-12-15 14:47:15 +0900

    ログイン画面作成

---
+++ src/resources/views/Auth/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Login</h2>
+<form action="/login" method="post">
+  @csrf
+  <div>
+    <label for="email">メールアドレス</label>
+      <input type="mail" name="email" id="email">
+  </div>
+  <p>
+    @error('email')
+    {{ $message }}
+    @enderror
+  </p>
+  <div>
+    <label for="password">パスワード</label>
+      <input type="password" name="password" id="password">
+  </div>
+  <p>
+    @error('password')
+    {{ $message }}
+    @enderror
+  </p>
+  <input type="submit" value="登録">
+</form>
+<a href="/register">Register</a>
+@endsection('content')
