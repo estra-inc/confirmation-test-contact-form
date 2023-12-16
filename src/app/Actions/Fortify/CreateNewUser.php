23108dd9fb9b22733a45807f68e7226412a6799a
ychihiro <y.chihiro0510@gmail.com>
2023-12-16 15:29:05 +0900

    fix: ログインバリデーション修正

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -33,14 +33,4 @@ class CreateNewUser implements CreatesNewUsers
             'password' => Hash::make($input['password']),
         ]);
     }
-
-    public function attributes()
-    {
-        return [
-            'name.required' => 'お名前を入力してください',
-            'email.required' => 'メールアドレスを入力してください',
-            'email.email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
-            'password.required' => 'パスワードを入力してください',
-        ];
-    }
 }
--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -3,15 +3,13 @@
 namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
-use App\Actions\Fortify\ResetUserPassword;
-use App\Actions\Fortify\UpdateUserPassword;
-use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+use App\Http\Requests\LoginRequest;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -43,5 +41,7 @@ class FortifyServiceProvider extends ServiceProvider
 
         return Limit::perMinute(10)->by($email . $request->ip());
         });
+
+        $this->app->bind(FortifyLoginRequest::class, LoginRequest::class);
     }
 }
