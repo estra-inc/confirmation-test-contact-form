e2c27514dfcd34abdcfc69f9b4612fd3427cda5f
ychihiro <y.chihiro0510@gmail.com>
2023-12-19 14:17:30 +0900

    fix: 管理画面表示のルーティングを修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,17 +1,6 @@
 <?php
-
-use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/admin', [AdminController::class, 'admin']);
+Route::get('/admin', function () {
+  return view('admin');
+});
