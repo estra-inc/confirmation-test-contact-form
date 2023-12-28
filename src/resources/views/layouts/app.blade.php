231a1bd88440cf82ff2d1bca16e158cf3a44c2f4
motoki_yuto <motoki.youto293@gmail.com>
2023-12-28 19:44:21 +0900

    feat: リセットCSS変更 use ress.css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FashionablyLate</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
   <link rel="stylesheet" href="{{ asset('css/common.css')}}">
   @yield('css')
-  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Inika&display=swap" rel="stylesheet"> -->
 </head>
+
 <body>
   <header class="header">
     <h1 class="header__heading">FashionablyLate</h1>
@@ -18,4 +17,5 @@
   </header>
   @yield('content')
 </body>
+
 </html>
