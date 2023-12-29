f3c71024d1050022e86296e98bdd13b6c90b32cf
ychihiro <y.chihiro0510@gmail.com>
2023-12-29 22:05:30 +0900

    fix: バリデーション修正

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -33,7 +33,7 @@ class ContactRequest extends FormRequest
             'tel_3' => 'required | max:5 | regex:/^[a-zA-Z0-9]+$/',
             'address' => 'required',
             'category_id' => 'required',
-            'detail' => 'required',
+            'detail' => 'required | max:120',
         ];
     }
 
@@ -42,7 +42,7 @@ class ContactRequest extends FormRequest
         return [
             'first_name.required' => '姓を入力してください',
             'last_name.required' => '名を入力してください',
-            'gender.required' => '性別を入力してください',
+            'gender.required' => '性別を選択してください',
             'email.required' => 'メールアドレスを入力してください',
             'email.email' => 'メールアドレスはメール形式で入力してください',
             'tel_1.required' => '電話番号を入力してください',
@@ -55,8 +55,9 @@ class ContactRequest extends FormRequest
             'tel_3.regex' => '電話番号は半角数字で入力してください',
             'tel_3.max' => '電話番号は5桁までの数字で入力してください',
             'address.required' => '住所を入力してください',
-            'category_id.required' => 'お問い合わせの種類を入力してください',
+            'category_id.required' => 'お問い合わせの種類を選択してください',
             'detail.required' => 'お問い合わせの内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
         ];
     }
 }
