ff81a50d5104e4e8056b3441169fb3101245644c
ychihiro <y.chihiro0510@gmail.com>
2023-12-31 00:24:11 +0900

    feat: サンクスページのUI作成

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,1 +1,18 @@
-<h1>サンクスページ</h1>
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+<link rel="stylesheet" href="{{ asset('css/thanks.css')}}">
+@endsection
+
+@section('content')
+<div class="thanks-page">
+  <div class="thanks-page__inner">
+    <p class="thanks-page__message">お問い合わせありがとうございました</p>
+    <button class="thanks-page__btn btn" href="">HOME</button>
+  </div>
+</div>
+<div class="thanks-page-bg__inner">
+  <span class="thanks-page-bg__text">Thank</span><span class="thanks-page-bg__text">you</span>
+</div>
+@endsection
