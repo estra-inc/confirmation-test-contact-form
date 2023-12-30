53f7d05122889012fe8e3afaa0771e33e0f7a4a3
ychihiro <y.chihiro0510@gmail.com>
2023-12-31 01:12:40 +0900

    feat: 管理画面の検索フォーム作成

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts/app')
 
 @section('content')
-<h2>Admin</h2>
+<div>
+  <h2>Admin</h2>
+  <div>
+    <form action="/search" method="post">
+      @csrf
+      <div>
+        <input type="text" name="keyword" placeholder="名前やメールアドレスを入力してください">
+        <div>
+          <select name="gender">
+            <option disabled selected>性別</option>
+            <option value="1">男性</option>
+            <option value="2">女性</option>
+            <option value="3">その他</option>
+          </select>
+        </div>
+        <div>
+          <select name="category_id">
+            <option disabled selected>お問い合わせの種類</option>
+            @foreach($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->content }}</option>
+            @endforeach
+          </select>
+        </div>
+        <div>
+          <input type="date" name="date">
+        </div>
+      </div>
+      <input type="submit" value="検索">
+      <input type="submit" value="リセット">
+    </form>
+  </div>
+  <table>
+    <tr>
+      <th>お名前</th>
+      <th>性別</th>
+      <th>メールアドレス</th>
+      <th>お問い合わせの種類</th>
+    </tr>
+  </table>
+
+
+</div>
+
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::post('/thanks', [ContactController::class, 'store']);
 Route::get('/admin', function () {
-  return view('admin');
+  $categories = Category::all();
+  return view('admin', compact('categories'));
 });
