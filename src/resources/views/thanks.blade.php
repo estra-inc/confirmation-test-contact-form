1bdb31cba5ec455d784957c49ad8103d67c5b21d
motoki_yuto <motoki.youto293@gmail.com>
2024-01-02 17:19:51 +0900

    style: リセットcss読み込み削除

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
 <link rel="stylesheet" href="{{ asset('css/thanks.css')}}">
 @endsection
 
