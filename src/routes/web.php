c38d4848ef2065a894d23f1b934c1a997a260208
JonyTask <yuusu.hyb@gmail.com>
2024-01-08 17:39:14 +0900

    不要なクラス読み込み削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\ContactController;
-use App\Models\Category;
-use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContactController::class, 'index']);
