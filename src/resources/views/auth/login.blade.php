9877d8eb2a5c0f75a79292fad7b1e0d15ac286dc
JonyTask <yuusu.hyb@gmail.com>
2024-05-07 16:05:59 +0900

    fix : login.blade placeholder

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
       @csrf
       <div class="login-form__group">
         <label class="login-form__label" for="email">メールアドレス</label>
-        <input class="login-form__input" type="mail" name="email" id="email">
+        <input class="login-form__input" type="mail" name="email" id="email" placeholder="例: test@example.com">
         <p class="register-form__error-message">
           @error('email')
           {{ $message }}
@@ -25,7 +25,7 @@
       </div>
       <div class="login-form__group">
         <label class="login-form__label" for="password">パスワード</label>
-        <input class="login-form__input" type="password" name="password" id="password">
+        <input class="login-form__input" type="password" name="password" id="password" placeholder="例: coachtech1106">
         <p>
           @error('password')
           {{ $message }}
